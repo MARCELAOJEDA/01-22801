8fee3dc9dfc55e2bcfbe75d5d1596d75cc601b40
MARCELAOJEDA <queso.en.la.red@gmail.com>
2022-09-12 18:23:26 -0300

    modifique

---
+++ js/02.js
@@ -0,0 +1,11 @@
+let miLet=0;
+var miVar=-1;
+
+const miDoc=document;
+
+
+console.log(miVar)
+console.log(miLet)
+console.log(miDoc)
+
+//problemas para q funcione el breack point y menu q despliega en document
