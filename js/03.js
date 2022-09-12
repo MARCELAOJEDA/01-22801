a290dbf29a0a25ac1a0a67ff6a0c04c900d54fee
MARCELAOJEDA <queso.en.la.red@gmail.com>
2022-09-12 18:57:13 -0300

    No funciono Inner..

--- js/03.js
+++ js/03.js
@@ -1,7 +1,7 @@
 const miDoc=document;
 console.log(miDoc)
 
-let miH1=miDoc.getEmentsByTagName("h6");
+let miH1=miDoc.getEmentsByTagName("H6");
 let miH2=miDoc.getEmentById("seraH2O");
 let miH3=miDoc.getElementsByClassName("tocoH3");
 let miH4=miDoc.getElementsByName();
@@ -9,3 +9,7 @@ let miH5=document.getSelection();
 console.log(miH1);
 console.log(miH2);
 console.log(miH3);
+miH3.innerText="modifico el texto";
+miH3.innerHTML="<button> Esto es un boton </button>";
+miH2.innerText="modifico el texto";
+miH2.innerHTML="<button> Esto es un boton </button>";
