f3c818be67e040a154ece5f1c9c218f6f7513a52
MARCELAOJEDA <queso.en.la.red@gmail.com>
2022-09-12 18:23:34 -0300

    modifique

---
+++ js/03.js
@@ -0,0 +1,11 @@
+const miDoc=document;
+console.log(miDoc)
+
+let miH1=miDoc.getEmentsByTagName("h6");
+let miH2=miDoc.getEmentById("seraH2O");
+let miH3=miDoc.getElementsByClassName("tocoH3");
+let miH4=miDoc.getElementsByName();
+let miH5=document.getSelection();
+console.log(miH1);
+console.log(miH2);
+console.log(miH3);
