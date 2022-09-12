4dde471782749043c0de688f4fe3b58093282b66
MARCELAOJEDA <queso.en.la.red@gmail.com>
2022-09-12 18:25:13 -0300

    modifique

---
+++ js/04.js
@@ -0,0 +1,10 @@
+class boton{
+    constructor(texto,color){
+        this.texto=texto;
+        this.color=color;
+    }
+
+    
+}
+const mi04=Document,getElementByID(mi04)
+let boton1= new boton("login","fff")
